a2564042001c5dc70b8d69f0474fa36c71e6c140
Artur <arturimankulov906@gmail.com>
2024-04-29 19:34:38 +0500

    create grid on main page

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,8 +1,36 @@
+import styles from "./MainPage.module.scss";
+import { Link } from "react-router-dom";
+
 const MainPage = () => {
   return(
-    <>
-      Главная страница
-    </>
+    <div className={styles.content}>
+      <div className={styles.component}>
+        <h1>Все статьи</h1>
+        <Link to={'/'}>Стиль</Link>
+        <Link to={'/'}>Отделы</Link>
+        <Link to={'/'}>Адреса офисов</Link>
+      </div>
+      <div className={styles.component}>
+        <h1>Недавние изменения</h1>
+        <Link to={'/'}>Дизайн компании</Link>
+        <Link to={'/'}>Адреса офисов</Link>
+        <Link to={'/'}>Должностные лица</Link>
+      </div>
+      <div className={styles.component}>
+        <h1>Создать</h1>
+        <Link to={'/'}>Таблица</Link>
+        <Link to={'/'}>Презентация</Link>
+        <Link to={'/'}>Отчет</Link>
+        <Link to={'/'}>Формула</Link>
+      </div>
+      <div className={styles.component}>
+        <h1>Популярное</h1>
+        <Link to={'/'}>Документы</Link>
+        <Link to={'/'}>Регламенты</Link>
+        <Link to={'/'}>Шаблоны</Link>
+        <Link to={'/'}>Часто задаваемые вопросы</Link>
+      </div>
+    </div>
   )
 }
 
