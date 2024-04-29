f127a711642116829c5621c41758fe073ef9ec7a
Artur <arturimankulov906@gmail.com>
2024-04-29 22:05:55 +0500

    added links

--- src/pages/desing/desing-components/DesingComponents.tsx
+++ src/pages/desing/desing-components/DesingComponents.tsx
@@ -4,9 +4,12 @@ import { Urls } from "@/app/constants/Urls.ts";
 const DesingComponents = () => {
   return (
     <div className={styles.content}>
-      <h1>Компоненты</h1>
-      <p>gegegegege</p>
-      <a href={Urls.COMPONENTS}>Ссылка на фигму</a>
+      <h1>Компоненты из дизайн системы</h1>
+      <p>Созданы для упрощение работы на стадии разработки, чтобы все проекты эко-системы Брусники были в одном стиле.</p>
+      <div className={styles.link}>
+        <p>Ссылка на Figma:</p>
+        <a href={Urls.COMPONENTS}>нажмите для перехода</a>
+      </div>
     </div>
   )
 }
